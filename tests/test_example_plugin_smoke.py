5d296c8570a9ad5e95b60d6fe1f2b23e986ec879
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-01-05 09:55:40 +0800

    feat(plugins): add example plugin

---
+++ tests/test_example_plugin_smoke.py
@@ -0,0 +1,7 @@
+from stonks_cli.plugins import load_plugins
+
+
+def test_example_plugin_loads():
+    registry = load_plugins(("asset/example_plugin.py",))
+    assert "example_sma20" in (registry.strategies or {})
+    assert "local_csv" in (registry.provider_factories or {})
